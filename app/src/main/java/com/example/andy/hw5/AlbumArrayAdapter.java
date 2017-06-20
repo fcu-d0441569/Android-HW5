bc0888127fd8854c9dead14cab5e93be8d9ddfbc
Ryan <jason8549@yahoo.com.tw>
2017-06-20 17:26:43 +0800

    555

---
+++ app/src/main/java/com/example/andy/hw5/AlbumArrayAdapter.java
@@ -0,0 +1,45 @@
+package com.example.andy.hw5;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by User on 2017/4/28.
+ */
+
+public class AlbumArrayAdapter extends ArrayAdapter<AlbumItem> {
+    Context context;
+    public AlbumArrayAdapter(Context context, ArrayList<AlbumItem> items) {
+        super(context, 0, items);
+        this.context = context;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        LinearLayout itemlayout = null;
+        if(convertView == null) {
+            itemlayout = (LinearLayout)inflater.inflate(R.layout.listitem, null);
+        } else {
+            itemlayout = (LinearLayout) convertView;
+        }
+
+        AlbumItem item = (AlbumItem)getItem(position);
+        TextView tv_name = (TextView)itemlayout.findViewById(R.id.itemtv);
+        tv_name.setText(item.name);
+        ImageView iv = (ImageView)itemlayout.findViewById(R.id.itemiv);
+        iv.setImageResource(item.imgid);
+        TextView tv_name2 = (TextView)itemlayout.findViewById(R.id.itemtv2);
+        tv_name2.setText(item.name2);
+        ImageView iv2 = (ImageView)itemlayout.findViewById(R.id.itemiv2);
+        iv2.setImageResource(item.imgid2);
+        return itemlayout;
+    }
+}
---
+++ app/src/main/java/com/example/andy/hw5/AlbumItem.java
@@ -0,0 +1,17 @@
+package com.example.andy.hw5;
+
+/**
+ * Created by Andy on 2017/6/17.
+ */
+
+
+public class AlbumItem {
+    String name,name2;
+    int imgid, imgid2;
+    AlbumItem(String name, int imgid,String name2, int imgid2) {
+        this.name = name;
+        this.imgid = imgid;
+        this.name2 = name2;
+        this.imgid2 = imgid2;
+    }
+}
